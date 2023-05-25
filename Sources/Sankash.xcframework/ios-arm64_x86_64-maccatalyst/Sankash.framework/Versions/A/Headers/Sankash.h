c0bf9fed341ebe150847e15052fd1f7e72dc9b5e
Vinod Bhaskar <vinodb@bigrattle.com>
2023-05-25 18:31:57 +0530

    initial commit

---
+++ Sources/Sankash.xcframework/ios-arm64_x86_64-maccatalyst/Sankash.framework/Versions/A/Headers/Sankash.h
@@ -0,0 +1,18 @@
+//
+//  Sankash.h
+//  Sankash
+//
+//  Created by Vinod Bhaskar on 25/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sankash.
+FOUNDATION_EXPORT double SankashVersionNumber;
+
+//! Project version string for Sankash.
+FOUNDATION_EXPORT const unsigned char SankashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sankash/PublicHeader.h>
+
+
